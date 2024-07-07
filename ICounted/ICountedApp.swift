9384bd4bcb1b849c15303733c0d02b2134068721
Nebo <antrabot@gmail.com>
2024-07-07 14:19:44 +0300

    Initial Commit

---
+++ ICounted/ICountedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICountedApp.swift
+//  ICounted
+//
+//  Created by Nebo on 07.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ICountedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
