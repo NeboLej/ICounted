5041cece0c0dbe52540d82ecc3c8fbc1b5863382
Nebo <kindersmail08@gmail.com>
2024-10-06 22:19:08 +0300

    added screen

---
+++ ICounted/🪇UI/CounterScreen/CounterScreenStore.swift
@@ -0,0 +1,37 @@
+//
+//  CounterScreenStore.swift
+//  ICounted
+//
+//  Created by Nebo on 06.10.2024.
+//
+
+import SwiftUI
+
+class CounterScreenStore: ObservableObject {
+    
+    @Published var name: String = ""
+    @Published var description: String = ""
+    @Published var color: Color = .blue
+    @Published var count: Int = 0
+    @Published var isUseTargetValue = false
+    @Published var targetCount: Int = 0
+    @Published var isAddToWidget = false
+    @Published var progress: Double = 0
+    
+    func bindCounter(counter: Counter) {
+        name = counter.name
+        description = counter.description
+        color = Color(hex: counter.colorHex)
+        count = counter.count
+        isUseTargetValue = counter.taggetCount != nil
+        targetCount =  counter.taggetCount != nil ?  counter.taggetCount! : 0
+        isAddToWidget = counter.isFavorite
+        
+        progress = getProgress()
+    }
+    
+    private func getProgress() -> Double {
+        if targetCount == 0 { return 1 }
+        return (100 / Double(targetCount)) * Double(count)
+    }
+}
