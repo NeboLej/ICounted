dd6947e9552aa71d1a52c20fb4a9c0f1946c4278
Nebo <kindersmail08@gmail.com>
2024-11-20 23:00:02 +0300

    fix crash

--- ICounted/Features/CounterList/Domain/CounterRecord.swift
+++ ICounted/Features/CounterList/Domain/CounterRecord.swift
@@ -13,9 +13,9 @@ class CounterRecord: Identifiable, Equatable {
     let id: UUID = UUID.init()
     let date: Date = Date()
     let message: String = "def"
-    let counter: Counter
+    let counter: Counter? = nil
     
-    init(id: UUID = .init(), date: Date = Date(), message: String = "", counter: Counter) {
+    init(id: UUID = .init(), date: Date = Date(), message: String = "", counter: Counter? = nil) {
         self.id = id
         self.date = date
         self.message = message
